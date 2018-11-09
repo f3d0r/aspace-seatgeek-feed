13723f3eb5bb9fede8287abbb6301450bb08ae60
Fedor Paretsky <me@f3d0r.com>
2018-11-08 22:05:36 -0800

    Add no-store cache header

--- app.js
+++ app.js
@@ -26,29 +26,29 @@ async function execute() {
 
 function getEvents(zipCodeInfo) {
     return new Promise(function (resolve, reject) {
-            var options = {
-                method: 'GET',
-                url: 'https://db_user.seatgeek.com/2/events',
-                qs: {
-                    lat: zipCodeInfo.lat,
-                    lon: zipCodeInfo.lng,
-                    per_page: '5000'
-            ***REMOVED***
-                json: true,
-                timeout: 30000,
-                headers: {
-                    authorization: 'Basic TVRNNE5qZzJPVFY4TVRVME1UY3pPVFEwTlM0eE53OjVjMzJlYzczYTg4YjgxZWNlYjMxYjU1NGE0YjgyOWY2M2Q4M2ZlNTMwMmY2MjgzOWQ1MzcyYWViNDlmZmM3YzE='
+        var options = {
+            method: 'GET',
+            url: 'https://db_user.seatgeek.com/2/events',
+            qs: {
+                lat: zipCodeInfo.lat,
+                lon: zipCodeInfo.lng,
+                per_page: '5000'
         ***REMOVED***
-    ***REMOVED***;
+            json: true,
+            timeout: 30000,
+            headers: {
+                'authorization': 'Basic TVRNNE5qZzJPVFY4TVRVME1UY3pPVFEwTlM0eE53OjVjMzJlYzczYTg4YjgxZWNlYjMxYjU1NGE0YjgyOWY2M2Q4M2ZlNTMwMmY2MjgzOWQ1MzcyYWViNDlmZmM3YzE=',
+                'Cache-Control': 'no-store'
+    ***REMOVED***
+***REMOVED***;
 
-            request(options, function (error, response, body) {
-                    console.log("PERCENT DONE: " + (100 * (currReq++) / totalReqs).toFixed(4) + "% ||  " + currReq + " OUT OF " + totalReqs);
-
-                if (error) {
-                    reject(error)
-        ***REMOVED*** else {
-                    resolve(body);
-        ***REMOVED***
-    ***REMOVED***);
+        request(options, function (error, response, body) {
+            console.log("PERCENT DONE: " + (100 * (currReq++) / totalReqs).toFixed(4) + "% ||  " + currReq + " OUT OF " + totalReqs);
+            if (error) {
+                reject(error)
+    ***REMOVED*** else {
+                resolve(body);
+    ***REMOVED***
+***REMOVED***);
     });
 }
